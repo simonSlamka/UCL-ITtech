b09d5f4c140c45276c2a7db91f0c49d84bbb7b5a
V1ncit <76640924+V1ncit@users.noreply.github.com>
2021-09-13 13:25:45 +0200

    Added todays exercises

--- programming/SimonStorm/ch2_ex2.py
+++
@@ -1,2 +0,0 @@
-name = str(input('What is your name? \n'))
-print('Heey', name ,',you are back you old son of a bitch!')
--- programming/SimonStorm/ch2_ex5.py
+++ programming/SimonStorm/ch2_ex5.py
@@ -2,9 +2,11 @@
 Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
 converted temperature.
 '''
+# Takes the users input
+celcius = input("Temperature in celcius: ")
 
-celcius = input('Temperature in celcius: ')
-
+# Converts the input to fahrenheit
 fahrenheit = celcius * 1.8 + 32.0
 
-print(fahrenheit)
+# Prints the result
+print(fahrenheit + "F")
