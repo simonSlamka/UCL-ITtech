225c3eebc2533b8e99f3822fa88c94b2e595ad6c
V1ncit <76640924+V1ncit@users.noreply.github.com>
2021-09-13 12:14:11 +0200

    Added solved excersises with correct namingconvention

---
+++ programming/SimonStorm/ch2_ex5.py
@@ -0,0 +1,10 @@
+'''
+Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
+converted temperature.
+'''
+
+celcius = input('Temperature in celcius: ')
+
+fahrenheit = celcius * 1.8 + 32.0
+
+print(fahrenheit)
