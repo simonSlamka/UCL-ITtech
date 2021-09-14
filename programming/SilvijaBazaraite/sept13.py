4153db578353193954a0d0fc49ff3b5a6c561f23
Silvija <siba36733@edu.ucl.dk>
2021-09-14 11:03:47 +0200

    I added comments to the file sept13

--- programming/SilvijaBazaraite/sept13.py
+++ programming/SilvijaBazaraite/sept13.py
@@ -1,16 +1,16 @@
-Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them
+#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them
 name = input ("What is your name?")
 What is your name?
  print ("Hello,put the name")
 
-Exercise 3: Write a program to prompt the user for hours and rate per
+#Exercise 3: Write a program to prompt the user for hours and rate per
 hour to compute gross pay.
  hourlyPay = int(input("put the hourly pay\n"))
 weeklyHours = int(input("put the weekly hours\n"))
 grossPay = str (hourlyPay * weeklyhours * 4)
 print ("gross monthly pay" + grossPay)
 
-Exercise 4: Assume that we execute the following assignment statements:
+#Exercise 4: Assume that we execute the following assignment statements:
 width = 17
 height = 12.0
 
@@ -19,6 +19,6 @@ print (width/2.0)
 print (height/3)
 print (1 + 2 * 5)
 
-Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
 converted temperature
 float(input("What is the Temperature\n"))
