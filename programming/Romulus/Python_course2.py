8b05279ddbe58b2da392eca4cfb01f5db57b30c8
Simon Slamka <simon.slamka@ongakken.com>
2021-09-14 06:45:42 +0000

    Updated Romulus' Python_course2.py

---
+++ programming/Romulus/Python_course2.py
@@ -0,0 +1,17 @@
+#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+# ask the user to imput his name
+name = input('What is your name_Hved hilla du?\n')
+print("welcome, " + name)
+# the user will see welcome and his name
+
+
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay
+# int means intingure because we want a number to store in the variable same fot Payrate
+Payhours = int(input('how many hours did you work this month\n?'))
+Payrate = int(input('how much are you payed per hour?\n'))
+# We take the number of hours and we multiply with how much he is payed per hour multipled with 4, in a month we have 4 weeks
+Payincome = str (Payhours * Payrate *4)
+print("Your monthly income is"+ Payincome)
+
+#Exercise 4: 
