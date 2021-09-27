189aa2940918805ed2eed5635d3e2ee4ea1b499b
Dominik Tarcsi <dominik.tarcsi99@gmail.com>
2021-09-27 09:46:10 +0200

    added files from chapter 3, added exercise from week 36,renamed my directory to remove spaces

---
+++ programming/dominikTarcsi/000_hello_world.py
@@ -0,0 +1,6 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world")
+Hello world
+>>> 
