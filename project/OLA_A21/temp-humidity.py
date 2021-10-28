e44d08e5f71f57f09e62722f7dec2859ae444a3b
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-10-28 10:16:39 +0200

    added the python file for the temp sensor

---
+++ project/OLA_A21/temp-humidity.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import dht11
+
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+sensorPin = dht11.DHT11(pin = 21)
+values = sensorPin.read()
+
+def getValues():
+    if values.is_valid():
+        print("Current temp:", values.temperature)
+        print("\n")
+        print("Current humidity:", values.humidity)
+    else:
+        print("Return values invalid! Check pinout!!", values.error_code)
+
+getValues()
