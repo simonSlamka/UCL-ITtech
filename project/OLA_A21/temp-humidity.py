aa00017489e89c352ec373d1af0ae3626822f57c
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-10-29 08:38:42 +0200

    fixedlogical errors in the temp-humidity.py file; fixed the API call URL, since I changed it on the server

--- project/OLA_A21/temp-humidity.py
+++ project/OLA_A21/temp-humidity.py
@@ -12,21 +12,21 @@ values = sensorPin.read()
 GPIO.setup(26, GPIO.OUT)
 
 def getValues():
-	while True:
-		if values.is_valid():
-			print("Current temp:", values.temperature)
-			print("\n")
-			if values.temperature >= 25:
-				GPIO.output(26, GPIO.HIGH)
-			else:
-				GPIO.output(26, GPIO.LOW)
-				print("Current humidity:", values.humidity)
+	if values.is_valid():
+		print("Current temp:", values.temperature)
+		print("\n")
+		if values.temperature >= 25:
+			GPIO.output(26, GPIO.HIGH)
 		else:
-			print("Return values invalid! Check pinout!!", values.error_code)	
-		sleep(5)
-		requests.post('http://auth.ongakken.com:2005/api/postMsgToDiscordByChannel', json={'msg': values.temperature})
+			GPIO.output(26, GPIO.LOW)
+			print("Current humidity:", values.humidity)
+	else:
+		print("Return values invalid! Check pinout!!", values.error_code)	
+	sleep(5)
+	requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', json={'msg': values.temperature})
 
-try:
-	getValues()
-except:
-	getValues()
+while True:
+	try:
+		getValues()
+	except:
+		getValues()
