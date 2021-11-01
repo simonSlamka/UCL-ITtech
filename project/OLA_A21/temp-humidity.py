524dfa76660d488cbe99c3ae10eecb52b07064ac
V1ncit <76640924+V1ncit@users.noreply.github.com>
2021-11-02 00:19:08 +0100

    Fixed issue where it would only post temperature, and not humidity. Also added timestamp and a 5 min delay between reads

--- project/OLA_A21/temp-humidity.py
+++ project/OLA_A21/temp-humidity.py
@@ -3,6 +3,7 @@ import dht11
 from time import sleep
 import requests
 import sys
+from datetime import datetime
 
 sys.stderr = object
 
@@ -14,11 +15,15 @@ sensorPin = dht11.DHT11(pin = 11)
 values = sensorPin.read()
 GPIO.setup(26, GPIO.OUT)
 
+def get_time_now():     # get system time
+    return datetime.now().strftime('    %H:%M:%S')
+
 def getValues():
 	if values.is_valid():
+		print(get_time_now())
 		print("Current temp:", values.temperature)
-		print("\n")
 		print("Current humidity:", values.humidity)
+		requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', headers={"Content-Type":"application/json"}, json={'msg': f"Temperature: {values.temperature}C \n Humidity: {values.humidity}%RH"})
 		if values.temperature >= 25:
 			GPIO.output(26, GPIO.HIGH)
 			requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', headers={"Content-Type":"application/json"}, json={"msg": "https://media.giphy.com/media/LMC8paGihNTuo/giphy.gif"})
@@ -27,11 +32,9 @@ def getValues():
 	else:
 		print("Return values invalid! Check pinout!!", values.error_code)
 		requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', headers={"Content-Type":"application/json"}, json={"msg": "Return values invalid! Check pinout!!"})
-	sleep(5)
-	requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', headers={"Content-Type":"application/json"}, json={'msg': f"Temp: {values.temperature}"})
-	requests.post('http://auth.ongakken.com:2005/api/postMsgToUCLchannelDiscord', headers={"Content-Type":"application/json"}, json={'msg': f"Humidity: {values.humidity}"})
-
+	sleep(600)
 while True:
+	print("Probing for temperature")
 	try:
 		getValues()
 	except KeyboardInterrupt:
